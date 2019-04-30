93990a1d970b0ed8c89f13425280bd1548069c63
Sebastian Dengler <dengler@toposens.de>
2019-04-30 08:38:53 +0200

    added travis files

---
+++ travis.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+
+# Run Docker container
+if ! [ "$IN_DOCKER" ]; then
+
+  docker pull $DOCKER_IMAGE
+
+  docker run \
+  -e IN_DOCKER=true \
+  -e TRAVIS_BRANCH \
+  -e TRAVIS_BUILD_DIR \
+  -v $(pwd):/root/$(basename $PWD) \
+  -v $HOME/.ccache:/root/.ccache \
+  -w /root/$(basename $PWD) \
+  -t \
+  $DOCKER_IMAGE /root/$(basename $PWD)/./$SCRIPT
+
+  exit
+fi
+
+
+# Display system information
+echo "##############################################"
+uname -a
+lsb_release -a
+gcc --version
+echo "CXXFLAGS = ${CXXFLAGS}"
+cmake --version
+echo "##############################################"
+
+
+# Setup ROS
+source /opt/ros/$(ls /opt/ros/)/setup.bash
+export CCACHE_DIR=/root/ccache
+
+
+# Prepare workspace
+PROJECT_NAME="testing"
+URL=${TRAVIS_BUILD_DIR/"/home/travis/build"/"https://github.com"}
+cd /root
+mkdir -p /catkin_ws/src
+git clone $URL -b $TRAVIS_BRANCH /catkin_ws/src/$PROJECT_NAME
+
+
+# Initialize git submodules
+cd /catkin_ws/src/$PROJECT_NAME
+git submodule update --init --recursive
+cd ../..
+
+
+# Lint
+catkin_lint -W3 .
+
+# Make
+catkin_make_isolated
+
+# Test
